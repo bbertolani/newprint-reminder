4fffdb675d38d20ccd1e19c7c948c8c2cab8cbb3
Bruno S Bertolani <brunosbertolani@gmail.com>
2020-02-28 22:12:30 -0300

    update

--- app/config/mongo.py
+++ app/config/mongo.py
@@ -1,14 +1,14 @@
-#from pymongo import MongoClient
+# from pymongo import MongoClient
 import sys
 
 from pymodm import connect
 
 
 def initMongo():
-  try:
-    client = connect('mongodb://host.docker.internal:27017/newprint')
-  except:
-    print("Mongo failed")
-    sys.exit(1)
+    try:
+        client = connect("mongodb://host.docker.internal:27017/newprint")
+    except:
+        print("Mongo failed")
+        sys.exit(1)
 
-  return client
+    return client

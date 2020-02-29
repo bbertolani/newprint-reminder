3aed08a3fedae8ef51a7f8ab0b14207f0674f1de
Bruno S Bertolani <brunosbertolani@gmail.com>
2020-02-28 22:11:59 -0300

    updated class to deal with new information updated scripts to use new class informations

--- app/models/reminder.py
+++ app/models/reminder.py
@@ -4,11 +4,16 @@ from config.mongo import initMongo
 
 initMongo()
 
+
 class Reminder(MongoModel):
-  order = fields.CharField()
-  part = fields.CharField()
-  email = fields.EmailField()
-  url = fields.CharField()
-  notification = fields.CharField()
-  status = fields.CharField()
-  createdAt = fields.DateTimeField()
+    origin = fields.CharField(required=True)
+    order_number = fields.IntegerField(required=True)
+    order_ID = fields.CharField(required=True)
+    item_ID = fields.IntegerField(blank=True)
+    email = fields.EmailField(required=True)
+    product_desc = fields.CharField(blank=True)
+    project_title = fields.CharField(blank=True)
+    url = fields.CharField(required=True)
+    notification = fields.IntegerField(required=True, choices=[0, 1, 2, 3])
+    status = fields.IntegerField(required=True, choices=[0, 1, 2, 3])
+    createdAt = fields.DateTimeField(required=True)
--- app/scripts/scripts.py
+++ app/scripts/scripts.py
@@ -10,55 +10,77 @@ from models.reminder import Reminder
 
 def getReminder():
     obj = Reminder.objects.all()
-    result = obj.only("order", "part").values()
+    result = obj.only("order_number", "order_ID", "item_ID").values()
     return dumps(result)
 
 
 def postReminder(request):
-    order = str(request.get("order"))
-    force = request.get("force")
-    notification = str(request.get("notification"))
-    part = str(request.get("part"))
+    origin = request.get("origin")
+    order_number = request.get("order_number")
+    order_ID = request.get("order_ID")
+    item_ID = request.get("item_ID")
     email = request.get("email")
+    product_desc = request.get("product_desc")
+    project_title = request.get("project_title")
     url = request.get("url")
+    notification = request.get("notification")
     status = request.get("status")
+    force = request.get("force")
     insert = False
 
     try:
-        Reminder.objects.get({"order": order, "part": part})
-        obj = {
+        Reminder.objects.get(
+            {"order_number": order_number, "order_ID": order_ID, "item_ID": item_ID}
+        )
+        msg = {
             "msg": "Order already waiting approval",
-            "order": order,
-            "part": part,
+            "order_number": order_number,
+            "order_ID": order_ID,
+            "item_ID": item_ID,
             "notification": notification,
         }
         if force:
-            Reminder.objects.get({"order": str(order)}).delete()
+            Reminder.objects.get(
+                {"order_number": order_number, "order_ID": order_ID, "item_ID": item_ID}
+            ).delete()
             insert = True
     except Reminder.DoesNotExist:
         insert = True
 
     if insert == True:
-        Reminder(
-            order,
-            part,
-            email,
-            url,
-            status,
-            notification,
-            createdAt=datetime.datetime.now(),
-        ).save()
-        obj = {
-            "order": order,
-            "part": part,
-            "msg": "Order is now waiting approval, First Notification",
-        }
+        try:
+            Reminder(
+                origin,
+                order_number,
+                order_ID,
+                item_ID,
+                email,
+                product_desc,
+                project_title,
+                url,
+                notification,
+                status,
+                createdAt=datetime.datetime.now(),
+            ).save()
+            msg = {
+                "order_number": order_number,
+                "order_ID": order_ID,
+                "item_ID": item_ID,
+                "msg": "Order is now waiting approval, First Notification",
+            }
+        except:
+            msg = {
+                "order_number": order_number,
+                "order_ID": order_ID,
+                "item_ID": item_ID,
+                "msg": "Order Failed",
+            }
 
-    return dumps(obj)
+    return dumps(msg)
 
 
 def putInfo(order, request):
-    part = str(request.get("part"))
+    part = request.get("part")
     status = request.get("status") or "None"
     notification = request.get("notification") or "None"
 

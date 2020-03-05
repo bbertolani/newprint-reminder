610f07da9c387ba35aa0864a1fe989567e529532
Bruno S Bertolani <brunosbertolani@gmail.com>
2020-03-05 08:11:02 -0300

    install of PTVSD for debug update routes and keep order informations in STRING

--- app/app.py
+++ app/app.py
@@ -5,25 +5,40 @@ from flask import Flask, request
 from scripts.scripts import postReminder, getReminder, putInfo, getInfo
 from scripts.trigger import triggerReminder
 
+
+try:
+    import ptvsd
+    
+    ptvsd.enable_attach(address=("0.0.0.0", 5050))
+    print("ptvsd is started")
+    ptvsd.wait_for_attach()
+
+except:
+    print("Failed or running....")
+
 app = Flask(__name__)
 
-@app.route('/api/reminder', methods=['GET', 'POST', 'PUT'])
+
+@app.route("/api/reminder", methods=["GET", "POST"])
 def methodsReminder():
-  if request.method == 'GET':
-    return getReminder()
-  if request.method == 'POST':
-    return postReminder(request.get_json())
+    if request.method == "GET":
+        return getReminder()
+    if request.method == "POST":
+        return postReminder(request.get_json())
 
-@app.route('/api/reminder/<order>', methods=['GET', 'PUT'])
-def methodsInfo(order):
-  if request.method == 'GET':
-    return getInfo(order, request.get_json())
-  if request.method == 'PUT':
-    return putInfo(order, request.get_json())
 
-@app.route('/api/triggerRemider', methods=['GET'])
+@app.route("/api/reminder/<order_number>/<order_ID>", methods=["GET", "PUT"])
+def methodsInfo(order_number, order_ID):
+    if request.method == "GET":
+        return getInfo(order_number, order_ID, request.get_json())
+    if request.method == "PUT":
+        return putInfo(order_number, order_ID, request.get_json())
+
+
+@app.route("/api/triggerRemider", methods=["GET"])
 def methodsTrigger():
-  return triggerReminder()
-  
+    return triggerReminder()
+
+
 if __name__ == "__main__":
-  app.run(host='0.0.0.0', port=8080, debug=True)
+    app.run(host="0.0.0.0", port=8080, debug=True)
